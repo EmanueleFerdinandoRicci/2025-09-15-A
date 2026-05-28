9352c4e980f81e00940cc57f1661e6aaa0816066
EmanueleFerdinandoRicci <emanueleferdinando.ricci@gmail.com>
2026-05-28 14:09:00 +0200

    Lezione 27/5 II metà

--- UI/controller.py
+++ UI/controller.py
@@ -13,12 +13,70 @@ class Controller:
         self._model.buildGraph(self._view._ddAnno1.value, self._view._ddAnno2.value)
         n,e = self._model.getGraphDetails()
         self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Grafo correttamente creato. Il grafo contiene {n} nodi e {e} archi")
+        )
         self._view.update_page()
 
     def handleDettagli(self, e):
-        pass
+        top3 = self._model.getTop3Archi()
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Archi di peso maggiore: (tre migliori)")
+        )
+        for arco in top3:
+            self._view.txt_result.controls.append(
+                ft.Text(f"{arco[0]} --> {arco[1]} Peso: {arco[2]["weight"]}")
+            )
+
+        numero, largest, details = self._model.getConnessaInfo()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Il grafo contiene {numero} componenti connesse")
+        )
+        self._view.txt_result.controls.append(
+            ft.Text(f"La componente connessa maggiore ha dimensione {len(largest)}")
+        )
+        for l in largest:
+            self._view.txt_result.controls.append(
+                ft.Text(l)
+            )
+        self._view.txt_result.controls.append(
+            ft.Text(f"Componente connessa in ordine decrescente di grado dei nodi")
+        )
+        for d in details:
+            self._view.txt_result.controls.append(
+                ft.Text(f"{d[0]} - grado: {d[1]}")
+            )
+        self._view.update_page()
+
+
     def handleCerca(self, e):
-        pass
+        k = self._view._txtInK.value
+        #controlli validità
+        kInt = int(k)
+
+        listPilotiOttima, minDistEta = self._model.getListaPilotiOttima(k)
+        if listPilotiOttima is None:
+            self._view.txt_result.controls.clear()
+            self._view.txt_result.controls.append(
+                ft.Text(f"Non ho trovato una lista di {k} piloti ottima per il numero di componenti connesse che ho")
+            )
+        self._view.txt_result.controls.clear()
+        self._view.txt_result.controls.append(
+            ft.Text(f"Lista di piloti con scarto di età minimo che non sono mai stati compagni di squadra nel range")
+        )
+        for p in listPilotiOttima:
+            self._view.txt_result.controls.append(
+                ft.Text(p)
+            )
+        self._view.txt_result.controls.append(
+            ft.Text(f"La differenza di età tra pilota più giovane e più anziano: {minDistEta}")
+        )
+        youngest = min(listPilotiOttima,key=lambda x:x.dob)
+        oldest = max(listPilotiOttima, key=lambda x: x.dob)
+        self._view.txt_result.controls.append(
+            ft.Text(f"Pilota più giovane: {youngest} - Pilota più anziano: {oldest}")
+        )
 
     def fillDDYear(self):
         years = self._model.getAllYears()
--- model/model.py
+++ model/model.py
@@ -1,3 +1,5 @@
+import copy
+
 import networkx as nx
 
 from database.DAO import DAO
@@ -8,6 +10,8 @@ class Model:
         self._graph = nx.Graph()
         self._drivers = []
         self._idMapD = {}
+        self._optListPiloti = []
+        self._minDistGiorni = 0
 
     def getAllYears(self):
         return DAO.getAllYears()
@@ -38,3 +42,49 @@ class Model:
 
         details = [(n,self._graph.degree(n)) for n in orderedNodes]
         return len(components), largest, details
+
+    def getListaPilotiOttima(self, k):
+        self._optListPiloti = []
+        self._minDistGiorni = 100*365
+
+        components = list(nx.connected_components(self._graph))
+
+        if len(components) < k:
+            # allora non ho abbastanza componenti connesse da cui pescare, e non posso trovare una sol
+            return None, 0
+
+        parziale = []
+        self._ricorsione(components, k, parziale,0 )
+        return self._optListPiloti, self._minDistGiorni
+
+    def _ricorsione(self, components, k, parziale, indexComponente):
+        # condizione di ottimalità
+        if len(parziale) == k:
+            # ho una soluzione accettabile.
+            dateDiNascita = [p.dob for p in parziale]
+            diffEtaPiloti = (max(dateDiNascita) - min(dateDiNascita)).days
+            if diffEtaPiloti < self._minDistGiorni:
+                self._optListPiloti = copy.deepcopy(parziale)
+                self._minDistGiorni = diffEtaPiloti
+            return
+
+        #condizione di terminazione
+        #1) esco se l'indice che indica quale comp connessa sto considerando a questa iterazione è diventato maggiore o
+        #uguale al numero di componenti connesse totali, perchè vuol dire che non ho altre componenti connesse da cui pescare
+        #2) l'altro motivo è se non ho abbastanza componenti rimanenti per arrivare a k piloti in parziale
+        if indexComponente >= len(components) or (len(components) - indexComponente) < (k-len(parziale)):
+            return
+
+        # se non sono uscito, allora posso aggiungere ancora piloti. Per questa componente, di indice indexComponente,
+        # provo ad ingaggiare un pilota oppure a non ingaggiare nessuno.
+
+        #caso 1, inserisco un pilota appartenente a questa comp connessa. In questo branch provo tutti i piloti che
+        # fanno parte della componente connessa in esame.
+        componente = components[indexComponente]
+        for pilota in componente:
+            parziale.append(pilota)
+            self._ricorsione(components, k, parziale, indexComponente+1)
+            parziale.pop()
+
+        #caso 2, mi tengo un brach di esplorazione in cui io non ho preso proprio nessuno da questa comonente.
+        self._ricorsione(components, k, parziale, indexComponente+1)
